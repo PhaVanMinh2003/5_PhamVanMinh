55bda0402a06516cab9187e0f81357486debb2db
PhaVanMinh2003 <phamminh.15092003@gmail.com>
2023-08-02 18:18:42 +0700

    submit bai1

---
+++ My SQL/Assignment 1/Demo2.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF exists QL_FTM;
+CREATE DATABASE QL_FTM;			/*Tạo database*/
+USE QL_FTM;						/*Chỉ đến DATABASE*/
+DROP TABLE  IF exists Trainee;	/*Kiểm tra sự tồn tại và xóa bảng*/
+CREATE TABLE Trainee			/*Lệnh tạo bảng*/
+(
+TraineeID 			INT  					NOT NULL	PRIMARY KEY,
+Full_Nametrainee 	NVARCHAR(50)    	 	NOT NULL,
+Birth_Date 			Date ,
+Gender 				BOOL					NOT NULL,
+ET_IQ    			INT(20)					NOT NULL,
+ET_Gmath 			INT (20) 				NOT NULL,
+ET_English  		INT(50)					NOT NULL,
+Training_Class 		VARCHAR(10)      		NOT NULL,
+Evaluation_Notes	NVARCHAR(50)        	NULL
+);
+/*Thêm cột "VTI_Account" có kiểu VARCHAR và mang giá một trường NOT NULL */
+ALTER 	TABLE Trainee
+		ADD  VTI_Account VARCHAR(20)  NOT NULL;
