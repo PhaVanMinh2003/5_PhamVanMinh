5b5120fd4ed64b52be2b37190000df66dc4e02ec
PhaVanMinh2003 <phamminh.15092003@gmail.com>
2023-08-07 11:25:36 +0700

    suybmit

--- My SQL/Assignment 1/Demo2.sql
+++ My SQL/Assignment 1/Demo2.sql
@@ -1,18 +1,17 @@
-DROP DATABASE IF exists QL_FTM;
-CREATE DATABASE QL_FTM;			/*Tạo database*/
-USE QL_FTM;						/*Chỉ đến DATABASE*/
-DROP TABLE  IF exists Trainee;	/*Kiểm tra sự tồn tại và xóa bảng*/
-CREATE TABLE Trainee (
-    TraineeID INT NOT NULL PRIMARY KEY,
-    Full_Nametrainee NVARCHAR(50) NOT NULL,
-    Birth_Date DATE,
-    Gender BOOL NOT NULL,
-    ET_IQ INT(20) NOT NULL,
-    ET_Gmath INT(20) NOT NULL,
-    ET_English INT(50) NOT NULL,
-    Training_Class VARCHAR(10) NOT NULL,
-    Evaluation_Notes NVARCHAR(50) NULL
+
+DROP DATABASE IF exists `fresher`;
+CREATE DATABASE `fresher`;
+CREATE TABLE `Trainee`
+(
+TraineeID			INT(10)AUTO_INCREMENT 					NOT NULL PRIMARY KEY,
+Full_Name			NVARCHAR(30) 							NOT NULL,
+Birth_Date  		DATE ,
+Gender				ENUM('male','female','unknown') 		NOT NULL,
+ET_IQ				TINYINT UNSIGNED CHECK (ET_IQ<=20)		NOT NULL,
+ET_Gmath			TINYINT UNSIGNED CHECK (ET_Gmath<=20)	NOT NULL,
+ET_English			TINYINT UNSIGNED CHECK (ET_English<=50)	NOT NULL,
+Training_Class		CHAR(10) 								NOT NULL,
+Evaluation_Notes	NVARCHAR(50) 							NOT NULL
 );
-/*Thêm cột "VTI_Account" có kiểu VARCHAR và mang giá một trường NOT NULL */
-ALTER 	TABLE Trainee
-		ADD  VTI_Account VARCHAR(20)  NOT NULL;
+ALTER TABLE `Trainee`
+			ADD COLUMN VTI_Account VARCHAR(30) UNIQUE NOT NULL
